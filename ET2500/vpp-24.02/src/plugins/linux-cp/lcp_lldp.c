3543a58f74348294d5ef684586e5bafc3f4b9a58
liyifei <liyifei@asterfusion.com>
2025-05-30 15:31:21 +0000

    solve ASROUTER-411-398:1.solve arp disable punt from l3-sub-interface 2.sovle lldp disable punt from bvi 3.solve bpdu diable punt from tap

--- ET2500/vpp-24.02/src/plugins/linux-cp/lcp_lldp.c
+++ ET2500/vpp-24.02/src/plugins/linux-cp/lcp_lldp.c
@@ -95,6 +95,14 @@ VLIB_NODE_FN (lcp_lldp_punt_node) (vlib_main_t * vm,
 
           //vnet_feature_next (&next0, b0);
           //vnet_feature_next (&next1, b1);
+        if (vnet_buffer2(b0)->l2_rx_sw_if_index > 0)
+          {
+                sw_if_index0 = vnet_buffer2(b0)->l2_rx_sw_if_index;
+          }
+          if (vnet_buffer2(b1)->l2_rx_sw_if_index > 0)
+          {
+                sw_if_index1 = vnet_buffer2(b1)->l2_rx_sw_if_index;
+          }
 
 	      lipi0 = lcp_itf_pair_find_by_phy (sw_if_index0); 
           if (lipi0 == INDEX_INVALID)
@@ -190,6 +198,11 @@ VLIB_NODE_FN (lcp_lldp_punt_node) (vlib_main_t * vm,
           //vnet_feature_next (&next0, b0);
 
           sw_if_index0 = vnet_buffer(b0)->sw_if_index[VLIB_RX];
+          if (vnet_buffer2(b0)->l2_rx_sw_if_index > 0)
+          {
+                sw_if_index0 = vnet_buffer2(b0)->l2_rx_sw_if_index;
+          }
+     
 
 	      lipi0 = lcp_itf_pair_find_by_phy (sw_if_index0); 
           if (lipi0 == INDEX_INVALID)
